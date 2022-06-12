27e72dedbcaa26016529e5ef62a765aa3f86c091
Ibrahim <87514639+IbrahimAnees@users.noreply.github.com>
2022-06-12 20:02:55 +1200

    Add files via upload

---
+++ download.py
@@ -0,0 +1,27 @@
+import youtube_dl
+import sys
+
+video_url = sys.argv[1]
+name = sys.argv[2]
+
+def run():
+    print(url)
+    print(name)
+    video_url = input("please enter youtube video url:")
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url = video_url,download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':"yo",
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete... {}".format(filename))
+
+if __name__=='__main__':
+    run()
